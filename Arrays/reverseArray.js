d224b69968eb7ea865fe9d131ef44548be0069cc
edubew <edubew98@gmail.com>
2024-05-31 00:24:26 +0300

    Write a function that takes an array and returns a new array with the elements in reverse order

---
+++ Arrays/reverseArray.js
@@ -0,0 +1,17 @@
+// Problem Statement: Write a function that takes an array and returns a new array with the elements in reverse order.
+
+// STEPS
+// 1. Declare and initialize the array variable
+// 2. Create an empty array to store the reversed array elements
+// 3. Using a for loop, iterate through the initial array starting from the last element to the first one
+// 4. On each iteration, add the current element to the new array
+// 5. After the loop ends, return the new array
+
+// SOLUTION
+function reverseArray(arr) {
+  let reversedArray = [];s
+  for (let i = arr.length - 1; i > 0; i--) {
+    reversedArray.push(arr[i]);
+  }
+  return reverseArray;
+}
